98a40b2e53168a15f1774331398f0bac8200c98d
Yoel Sherwin <yoel.sherwin@gmail.co.il>
2020-12-28 16:32:28 +0200

    some fixes

--- core/Country.py
+++ core/Country.py
@@ -9,16 +9,10 @@ class Country():
         self.data = get_country_by_id(id)
         self.city = get_random_city_from_list_by_id(id)
 
-    """
-        Generate the lists of questions and cities
-    """
-
-
 
     """
-        Get locations by city
+        Get locations of city
     """
-
     def get_locations(self):
         NUMBER_OF_LOCATIONS = 10
         all_locations = get_locations_by_city_name(self.get_src_city)
@@ -28,12 +22,13 @@ class Country():
             chosen_locations.append(all_locations[idx])
         return chosen_locations
 
+
     """
         Get string description of country
     """
-
     def get_description(self):
         return get_country_description(self.data)
+
 
     def get_map(self):
         map = self.data[1].lower()
@@ -41,17 +36,22 @@ class Country():
         map = map + '.png'
         return map
 
+
     def get_country_name(self):
         return self.data[1]
+
 
     def get_src_city(self):
         return self.city[1]
 
+
     def get_src_city_id(self):
         return self.city[0]
 
+
     def get_flag(self):
         return self.data[6]
+
 
     def get_id(self):
         return self.id
